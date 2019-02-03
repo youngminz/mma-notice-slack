98fc8491dec5f45409d8b4dfa5b60e4d3ceac29e
Youngmin Koo <youngminz.kr@gmail.com>
2019-02-03 17:44:32 +0900

    feat: 공지사항 모델 생성

--- notice/models.py
+++ notice/models.py
@@ -1,3 +1,23 @@
+from model_utils.models import TimeStampedModel
+
 from django.db import models
 
-# Create your models here.
+
+class Notice(TimeStampedModel):
+    serial_number = models.CharField(max_length=20, db_index=True)
+
+    title = models.CharField(max_length=100)
+    writer = models.CharField(max_length=20)
+    date = models.DateField()
+    view = models.IntegerField()
+    content = models.TextField()
+
+
+class Attachment(TimeStampedModel):
+    notice = models.ForeignKey(Notice, on_delete=models.CASCADE)
+
+    file_name = models.CharField(max_length=100)
+    file_size = models.IntegerField(null=True)
+    file_hash = models.CharField(max_length=64)
+
+    downloaded = models.BooleanField()
